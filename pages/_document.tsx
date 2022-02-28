d5237f2a07283a411099232f9c54782bfd83f231
farhanmian <farhanmian099@gmail.com>
2022-02-28 22:33:57 +0530

    fixed a bug

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,13 +3,14 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheets } from "@material-ui/core/styles";
 
 class MyDocument extends Document {
-  static async getInitialProps(ctx) {
+  static async getInitialProps(ctx: any) {
     const sheets = new ServerStyleSheets();
     const originalRenderPage = ctx.renderPage;
 
     ctx.renderPage = () =>
       originalRenderPage({
-        enhanceApp: (App) => (props) => sheets.collect(<App {...props} />)
+        enhanceApp: (App: any) => (props: any) =>
+          sheets.collect(<App {...props} />),
       });
 
     const initialProps = await Document.getInitialProps(ctx);
@@ -19,8 +20,8 @@ class MyDocument extends Document {
       // Styles fragment is rendered after the app and page rendering finish.
       styles: [
         ...React.Children.toArray(initialProps.styles),
-        sheets.getStyleElement()
-      ]
+        sheets.getStyleElement(),
+      ],
     };
   }
 
@@ -30,12 +31,17 @@ class MyDocument extends Document {
         <Head>
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Fruktur&display=swap" rel="stylesheet" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Fruktur&display=swap"
+            rel="stylesheet"
+          />
 
           <link rel="preconnect" href="https://fonts.googleapis.com" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
-          <link href="https://fonts.googleapis.com/css2?family=Zen+Kurenaido&display=swap" rel="stylesheet"></link>
-
+          <link
+            href="https://fonts.googleapis.com/css2?family=Zen+Kurenaido&display=swap"
+            rel="stylesheet"
+          ></link>
         </Head>
         <body>
           <Main />
